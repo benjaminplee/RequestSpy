a502e13841a5b1be3a529ebcd2ade602923a6089
Benjamin P Lee <leebenjp@gmail.com>
2009-11-20 13:34:41 -0600

    logging and readme

--- src/yardspoon/requestspy/RequestServlet.java
+++ src/yardspoon/requestspy/RequestServlet.java
@@ -2,6 +2,7 @@ package yardspoon.requestspy;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class RequestServlet extends HttpServlet {
 	
+	private static final Logger log = Logger.getLogger(RequestServlet.class.getName());
+	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		log.info("Incoming request.");
+		
 		// Record tx by path
 		
 		respondWithDate(response);
