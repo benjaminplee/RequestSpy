358ac8c3e8c207adfc294926526781db76d29e9e
Benjamin P Lee <leebenjp@gmail.com>
2009-11-20 10:48:18 -0600

    initial commit with working url mapping

---
+++ src/yardspoon/requestspy/SpyServlet.java
@@ -0,0 +1,13 @@
+package yardspoon.requestspy;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SpyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Spy");
+	}
+}
