992faaf98aadc4c738d9f484f84b26f866f7ed7a
Benjamin P Lee <leebenjp@gmail.com>
2009-11-22 19:55:11 -0600

    working v1.0, requests and spies work and deployed to AppEngine

--- src/yardspoon/requestspy/SpyServlet.java
+++ src/yardspoon/requestspy/SpyServlet.java
@@ -1,18 +1,64 @@
 package yardspoon.requestspy;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class SpyServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	
+	private static final Logger log = Logger.getLogger(SpyServlet.class.getName());
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 		
-		// Lookup tx to this URL
+		String spiedPath = request.getPathInfo();
+		log.info("Spying requests for path: " + spiedPath);
 		
-		// Render page with results
+		PersistenceManager persistenceManager = null;
+		Query query = null;
+		List<SpiedRequest> results = null;
+
+		try {
+			persistenceManager = PMF.get().getPersistenceManager();
+			query = persistenceManager.newQuery(SpiedRequest.class);
+
+			query.setFilter("path == spiedPath");
+		    query.setOrdering("when desc");
+		    query.declareParameters("String spiedPath");
+
+			results = (List<SpiedRequest>) query.execute(spiedPath);
+			
+	        if (results.iterator().hasNext()) {
+	            log.fine("Found " + results.size() + " spied requests for: " + spiedPath);
+	        } else {
+	            log.fine("No Spied Requests found for: " + spiedPath);
+	        }
+	    } finally {
+	    	if(query != null) { query.closeAll(); }
+	    	if(persistenceManager != null) { persistenceManager.close(); }
+	    }
 		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Spy");
+	    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/spy.jsp");
+	    try {
+	    	request.setAttribute("spiedPath", spiedPath);
+	    	request.setAttribute("spiedRequests", results);
+			requestDispatcher.forward(request, response);
+		} catch (ServletException e) {
+			log.warning(e.getMessage());
+			
+			response.setContentType("text/plain");
+			response.getWriter().println("Error retrieving Spy for path: " + spiedPath);
+		}
 	}
 }
---
+++ war/spy.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" isELIgnored="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>RequestSpy - Spied on PATH</title>
+</head>
+<body>
+	<h1>Spied Requests for ${ requestScope.spiedPath }</h1>
+	<ul>
+	<c:forEach var="spiedRequest" items="${ requestScope.spiedRequests }">
+		<li>${ spiedRequest }</li>	
+	</c:forEach>
+	</ul>	
+</body>
+</html>
